c01b91a17cc691a1f9fdce7045bc8c4dae7a1191
Mineta Hatsune <hatsunem@MinetanoMacBook-Pro.local>
2018-12-05 15:47:01 +0900

    Ceate ヘッダー

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div className="intro-bar">
+          <p>Wantedlyは、月間200万人が利用する国内最大のビジネスSNSです</p>
+        </div>
+        <div>
+          <header>
+            <div className="logo-and-search">
+              <p>
+                <img alt="Wantedly" src="https://de7iszmjjjuya.cloudfront.net/assets/beta/header-logo-loggedout-0d3e0bf2ed7533fe5ace8e585e3b6dafe93a13ec41f0f9c396d28435a35e9a6a.png"></img>
+              </p>
+              <form className="form-group">
+                <input type="text" className="form-control" placeholder="人や会社、募集を検索"></input>
+              </form>
+            </div>
+            <div className="menu">
+              <ul>
+                <li>採用担当者の方はこちら</li>
+                <li>Facebookで無料登録</li>
+                <li>ログイン</li>
+              </ul>
+            </div>
+          </header>
+        </div>
       </div>
     );
   }
